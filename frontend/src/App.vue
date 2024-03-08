701060ee53a84a8afd3768d9df0bab9c7ca933d4
dk <pivottocoding@gmail.com>
2024-03-08 14:41:08 +0900

    [Feat] HeaderComponent 개발 (#45) - 헤더컴포넌트개발 - 버튼 구성 시 아이콘 대체 필요 Fixes: #45

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,16 @@
 <template>
+  <HeaderComponent></HeaderComponent>
   <router-view></router-view>
 </template>
 
 <script>
+import HeaderComponent from './component/HeaderComponent.vue';
+
 export default {
   name: 'App',
+  components:{
+    HeaderComponent
+  }
 }
 </script>
 
@@ -15,6 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
