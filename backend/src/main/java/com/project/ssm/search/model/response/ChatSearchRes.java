a6bbdbaa4185115f29d5bfd1d72851eed1e5ee08
Hosae0905 <didghtls11@gmail.com>
2024-03-16 14:34:09 +0900

    [Feat] 채팅 검색 기능 개발 (#30)
    - 채팅 검색 기능 개발 (#30)

--- backend/src/main/java/com/project/ssm/search/model/response/ChatSearchRes.java
+++ backend/src/main/java/com/project/ssm/search/model/response/ChatSearchRes.java
@@ -2,11 +2,19 @@ package com.project.ssm.search.model.response;
 
 import lombok.*;
 
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class ChatSearchRes {
-//
-//}
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatSearchRes {
+    private String memberId;
+    private String message;
+
+    public static ChatSearchRes buildChatSearch(String memberId, String message) {
+        return ChatSearchRes.builder()
+                .memberId(memberId)
+                .message(message)
+                .build();
+    }
+}
