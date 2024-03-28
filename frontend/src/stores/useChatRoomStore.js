8ca4c28727084dff1a04c9e6752ecae1ee1b5e30
Hosae0905 <didghtls11@gmail.com>
2024-03-28 14:08:55 +0900

    [Refactor] 에러 페이지 수정 및 예외 처리 추가
    - 라우터를 사용하여 에러 페이지의 에러 코드 및 설명 추가
    - 채팅방 조회 에러 발생시 예외 처리

--- frontend/src/stores/useChatRoomStore.js
+++ frontend/src/stores/useChatRoomStore.js
@@ -1,6 +1,8 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 import {toRaw} from "vue";
+import { useRouter } from "vue-router";
+
 // const backend = 'http://192.168.0.41/api'
 const backend = 'http://localhost:8080';
 
@@ -12,16 +14,23 @@ export const useChatRoomStore = defineStore("chatRoom", {
     }),
     actions: {
         async getRoomList() {
-            let response = await axios.get(`${backend}/chat/rooms`, {
-                headers: {
-                    Authorization: localStorage.getItem("accessToken")
-                },
-            });
-            if (response.data.result !== null) {
-                response.data.result.forEach((chatRoom) => {
-                    this.roomList.push(chatRoom);
-                })
-                return toRaw(this.roomList);
+            const router = useRouter();
+            try {
+                let response = await axios.get(`${backend}/chat/rooms`, {
+                    headers: {
+                        Authorization: localStorage.getItem("accessToken")
+                    },
+                });
+                if (response.data.result !== null) {
+                    response.data.result.forEach((chatRoom) => {
+                        this.roomList.push(chatRoom);
+                    })
+                    return toRaw(this.roomList);
+                }
+            } catch (error) {
+                console.log(error.response.status);
+                console.log(error.response.data.message);
+                router.push({name: 'error', params: {errorStatus: error.response.status, message: error.response.data.message}});
             }
         },
     }
