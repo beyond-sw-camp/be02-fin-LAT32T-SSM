99a20f570337020b368c39e67a8e762fa9de1975
Hosae0905 <didghtls11@gmail.com>
2024-03-29 20:31:06 +0900

    [Refactor] x-frame-options 설정 변경
    - x-frame-options 설정 변경

--- backend/src/main/java/com/project/ssm/member/config/SecurityConfig.java
+++ backend/src/main/java/com/project/ssm/member/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -34,7 +35,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         try {
-            http.csrf().disable()
+            http.headers().frameOptions().disable()
+                    .addHeaderWriter(new StaticHeadersWriter("X-FRAME-OPTIONS", "SAME_ORIGIN http://192.168.0.21"))
+                    .and()
+                    .csrf().disable()
                     .authorizeHttpRequests()
                     .antMatchers("/**").permitAll()
                     .anyRequest().authenticated()
