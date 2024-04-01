87384701640f9c102bb3d895b3625bf0d0a5e5e0
jyk147369 <jyk147369@gmail.com>
2024-04-01 20:44:48 +0900

    [Refactor] 일정 기능 리팩토링 - 더미 데이터 테스트

--- backend/src/main/java/com/project/ssm/notification/Scheduler.java
+++ backend/src/main/java/com/project/ssm/notification/Scheduler.java
@@ -14,7 +14,7 @@ public class Scheduler {
 
 
 //     10초에 한번씩 메서드 실행
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedRate = 300000)
     public void schedule(){
         notificationService.memberEventRead();
     }
