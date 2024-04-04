a9b731b8f3d543d14a120ec63b730b5feb4269b6
poil4291 <poil4291@gmail.com>
2024-04-04 11:56:11 +0900

    [ãRepactor] 배치 리펙토링 -멤버 조회ë 후 저장한 멤ã버를 emitter로 전달이 안됨

--- backend/ssm-batch/src/main/java/com/project/batch/job/sendScheduleAlarmJobConfiguration.java
+++ backend/ssm-batch/src/main/java/com/project/batch/job/sendScheduleAlarmJobConfiguration.java
@@ -13,9 +13,16 @@ import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,26 +36,10 @@ public class sendScheduleAlarmJobConfiguration {
 
     @Bean
     public Job sendAlarmJob(
-            Step sendEngagementAlarmStep,
             Step sendEventAlarmStep
     ) {
         return jobBuilderFactory.get("sendAlarmJob")
-                .start(sendEngagementAlarmStep)
-                .next(sendEventAlarmStep)
-                .build();
-    }
-
-    @Bean
-    public Step sendEngagementAlarmStep(
-            ItemReader<sendScheduleAlarmReq> sendEngagementAlarmReader,
-            ItemWriter<sendScheduleAlarmReq> sendAlarmWriter
-    ) {
-        // Reader와 Writer를 사용하여 처리
-        return stepBuilderFactory.get("sendEngagementAlarmStep")
-                .<sendScheduleAlarmReq, sendScheduleAlarmReq>chunk(CHUNK_SIZE)
-                .reader(sendEngagementAlarmReader)
-                .writer(sendAlarmWriter)
-                .allowStartIfComplete(true) // 이전 실행이 완료되었어도 시작 가능
+                .start(sendEventAlarmStep)
                 .build();
     }
 
@@ -57,44 +48,55 @@ public class sendScheduleAlarmJobConfiguration {
             ItemReader<sendScheduleAlarmReq> sendScheduleAlarmReader,
             ItemWriter<sendScheduleAlarmReq> sendAlarmWriter
     ) {
+        // Reader와 Writer를 사용하여 처리
         return stepBuilderFactory.get("sendEventAlarmStep")
                 .<sendScheduleAlarmReq, sendScheduleAlarmReq>chunk(CHUNK_SIZE)
                 .reader(sendScheduleAlarmReader)
                 .writer(sendAlarmWriter)
-                .allowStartIfComplete(true)
+                .allowStartIfComplete(true) // 이전 실행이 완료되었어도 시작 가능
                 .build();
     }
 
-    @Bean
-    public JdbcCursorItemReader<sendScheduleAlarmReq> sendEngagementAlarmReader() {
-        // 참여 요청이 수락된 사용자에 대한 정보를 읽어오는 Reader
-        return new JdbcCursorItemReaderBuilder<sendScheduleAlarmReq>()
-                .dataSource(dataSource)
-                // started_at 형변환.
-                .rowMapper(new BeanPropertyRowMapper<>(sendScheduleAlarmReq.class))
-                .sql("SELECT event.* " +
-                        "FROM event " +
-                        "LEFT JOIN event_participants ON event.event_idx = event_participants.event_idx " +
-                        "LEFT JOIN member ON event_participants.member_idx = member.member_idx " +
-                        "WHERE event.started_at >= DATE_ADD(NOW(), INTERVAL 9 HOUR) + INTERVAL 1 MINUTE " +
-                        "AND event.started_at <= DATE_ADD(NOW(), INTERVAL 9 HOUR) + INTERVAL 10 MINUTE ")
-                .name("jdbcCursorItemReader")
-                .build();
-    }
-
-    // 일정 작성자에게 알람을 보내기 위한 정보를 데이터베이스에서 읽어오는 Reader
     @Bean
     public JdbcCursorItemReader<sendScheduleAlarmReq> sendScheduleAlarmReader() {
+        // 일정 참여 요청이 수락된 사용자에 대한 정보를 읽어오는 Reader
         return new JdbcCursorItemReaderBuilder<sendScheduleAlarmReq>()
                 .dataSource(dataSource)
-                // started_at 형변환.
-                .rowMapper(new BeanPropertyRowMapper<>(sendScheduleAlarmReq.class))
-                .sql("SELECT event.* " +
+                .rowMapper(new RowMapper<sendScheduleAlarmReq>() {
+                    @Override
+                    public sendScheduleAlarmReq mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+                        sendScheduleAlarmReq req = new sendScheduleAlarmReq();
+                        // started_at 컬럼의 문자열 값을 LocalDateTime으로 변환하여 설정
+                        String startedAtString = resultSet.getString("started_at");
+                        LocalDateTime startedAt = null;
+                        try {
+                            // 초까지 포함
+                            startedAt = LocalDateTime.parse(startedAtString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                        } catch (DateTimeParseException e) {
+                            // 만약 초까지 파싱이 실패하면 다른 형식으로 다시 시도
+                            try {
+                                // 분까지만
+                                startedAt = LocalDateTime.parse(startedAtString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+                            } catch (DateTimeParseException ex) {
+                                // 그래도 없으면
+                                startedAt = null;
+                            }
+                        }
+                        req.setStartedAt(startedAt);
+                        req.setMemberId(resultSet.getString("member_id"));
+                        // 멤버 받아오는지 확인
+                        // System.out.println(req.getMemberId());
+                        return req;
+                    }
+                })
+                // 참가 테이블을 통해 멤버 idx를 조회해온다.
+                .sql("SELECT event.*, member.member_id " +
                         "FROM event " +
                         "LEFT JOIN event_participants ON event.event_idx = event_participants.event_idx " +
-                        "LEFT JOIN member ON member.member_idx = event.event_maker " + // 작성자 정보 조인
+                        "LEFT JOIN member ON event_participants.member_idx = member.member_idx " +  // 작성자 정보 조인
                         "WHERE event.started_at >= DATE_ADD(NOW(), INTERVAL 9 HOUR) + INTERVAL 1 MINUTE " + // 10분안에 시작하는 모든 일정 선택
-                        "AND event.started_at <= DATE_ADD(NOW(), INTERVAL 9 HOUR) + INTERVAL 10 MINUTE; ")
+                        "AND event.started_at <= DATE_ADD(NOW(), INTERVAL 9 HOUR) + INTERVAL 10 MINUTE "
+                )
                 .name("jdbcCursorItemReader")
                 .build();
     }
@@ -103,11 +105,15 @@ public class sendScheduleAlarmJobConfiguration {
     @Bean
     public ItemWriter<sendScheduleAlarmReq> sendAlarmWriter() {
         return list -> {
+            List<String> memberIds = new ArrayList<>();
+            String message = null;
             for (sendScheduleAlarmReq req : list) {
-                String memberId = req.getMemberId();
-                String message = "당신의 \"" + req.getTitle() + "\" 일정이 시작전입니다.";
-                NotificationController.sendAlarmToClients(memberId, message);
+                message = "당신의 " + req.getTitle() +  " 일정이 시작전입니다.";
+                memberIds.add(req.getMemberId());
+                System.out.println(req.getMemberId());
             }
+            NotificationController.sendAlarmToClients(memberIds, message);
         };
     }
 }
+
--- backend/ssm-batch/src/main/java/com/project/batch/notification/NotificationController.java
+++ backend/ssm-batch/src/main/java/com/project/batch/notification/NotificationController.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,19 +19,24 @@ public class NotificationController {
 
     private static final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
 
-
-
-
     // 알람 발생 시 모든 클라이언트에게 알람 전송
-    public static void sendAlarmToClients(String memberId ,String message) {
-        SseEmitter emitter = emitters.get(memberId);
-        if (emitter != null) {
-            try {
-                emitter.send(SseEmitter.event().name("notification").data(message));
-            } catch (IOException e) {
-                emitters.remove(memberId);
+    public static void sendAlarmToClients(List<String> memberIds, String message) {
+        for (String memberId : memberIds) {
+            System.out.println(emitters.get(memberId));
+            SseEmitter emitter = emitters.get(memberId);
+            if (emitter != null) {
+                try {
+                    emitter.send(SseEmitter.event().name("notification").data(message));
+                } catch (IOException e) {
+                    emitters.remove(memberId);
+                }
             }
         }
+    }
+
+    @PostConstruct
+    private void init() {
+        emitters.put("default", new SseEmitter());
     }
 
 
