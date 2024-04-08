cfa0e35337decb5437915835b91da479214d001e
hosin <didghtls11@gmail.com>
2024-04-08 20:47:05 +0900

    [Refactor] CORS 관리를 위한 WebConfig 개발 (#182)
    - 채널 인터셉터 부분 삭제
    - WebConfig 설정 마무리

--- backend/src/main/java/com/project/ssm/config/web/WebSocketBrokerConfig.java
+++ backend/src/main/java/com/project/ssm/config/web/WebSocketBrokerConfig.java
@@ -1,10 +1,8 @@
 package com.project.ssm.config.web;
 
-import com.project.ssm.chat.interceptor.WebSocketBrokerInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -15,8 +13,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final WebSocketBrokerInterceptor interceptor;
-
     @Value("${address.ws}")
     private String wsAddress;
 
@@ -24,11 +20,6 @@ public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat").setAllowedOriginPatterns(wsAddress);
 
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(interceptor);
     }
 
     @Override
