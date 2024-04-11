8135ca299eb1dc69478f662879feca0c9d75c8a4
jyk147369 <jyk147369@gmail.com>
2024-04-12 01:56:54 +0900

    [Refactor] 에러 코드 리팩토링

--- backend/src/main/java/com/project/ssm/events/exception/ReservationAccessException.java
+++ backend/src/main/java/com/project/ssm/events/exception/ReservationAccessException.java
@@ -10,7 +10,6 @@ public class ReservationAccessException extends EntityNotFoundException {
     public static ReservationAccessException forMemberAmount(Integer memberAmount) {
         return new ReservationAccessException(ErrorCode.RESERVATION_ACCESS, "인원이 초과 되었습니다.");
     }
-
     public static ReservationAccessException forReservationTime() {
         return new ReservationAccessException(ErrorCode.RESERVATION_ACCESS, "종료 시간은 시작시간보다 앞설 수 없습니다.");
     }
